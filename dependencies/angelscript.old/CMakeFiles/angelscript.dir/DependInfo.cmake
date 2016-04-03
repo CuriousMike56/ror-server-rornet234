07c2362f44b3f3b46fcba5c06b7c045c15877afa
Hotrod55 <Hotrod55NFM@gmail.com>
2016-04-03 01:49:00 -0400

    Uploaded server files

---
+++ dependencies/angelscript.old/CMakeFiles/angelscript.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rorserver/bin/0.37/dependencies/angelscript/add_on/scriptdictionary/scriptdictionary.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/add_on/scriptdictionary/scriptdictionary.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/add_on/scriptmath/scriptmath.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/add_on/scriptmath/scriptmath.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/add_on/scriptmath3d/scriptmath3d.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/add_on/scriptmath3d/scriptmath3d.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/add_on/scriptstdstring/scriptstdstring.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/add_on/scriptstdstring/scriptstdstring.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/add_on/scriptstring/scriptstring.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/add_on/scriptstring/scriptstring.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/add_on/scriptstring/scriptstring_utils.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/add_on/scriptstring/scriptstring_utils.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_arrayobject.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_arrayobject.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_atomic.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_atomic.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_builder.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_builder.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_bytecode.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_bytecode.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_callfunc.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_callfunc.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_callfunc_mips.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_callfunc_mips.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_callfunc_ppc.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_callfunc_ppc.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_callfunc_ppc_64.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_callfunc_ppc_64.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_callfunc_sh4.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_callfunc_sh4.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_callfunc_x64_gcc.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_callfunc_x64_gcc.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_callfunc_x86.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_callfunc_x86.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_callfunc_xenon.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_callfunc_xenon.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_compiler.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_compiler.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_configgroup.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_configgroup.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_context.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_context.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_datatype.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_datatype.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_gc.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_gc.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_generic.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_generic.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_memory.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_memory.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_module.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_module.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_objecttype.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_objecttype.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_outputbuffer.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_outputbuffer.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_parser.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_parser.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_restore.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_restore.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_scriptcode.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_scriptcode.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_scriptengine.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_scriptengine.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_scriptfunction.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_scriptfunction.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_scriptnode.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_scriptnode.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_scriptstruct.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_scriptstruct.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_string.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_string.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_string_util.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_string_util.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_thread.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_thread.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_tokenizer.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_tokenizer.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_typeinfo.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_typeinfo.cpp.o"
+  "/home/rorserver/bin/0.37/dependencies/angelscript/source/as_variablescope.cpp" "/home/rorserver/bin/0.37/dependencies/angelscript/CMakeFiles/angelscript.dir/source/as_variablescope.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "dependencies/angelscript/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
